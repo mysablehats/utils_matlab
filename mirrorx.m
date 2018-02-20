75d629e809f2071de06db91f9c0c9400a270e0bb
mysablehats <mysablehats@gmail.com>
2018-02-20 13:57:13 +0000

    added support for sliding windows.

--- mirrorx.m
+++ mirrorx.m
@@ -1,11 +1,15 @@
 function newskel = mirrorx(tdskel, skelldef)
 newskel1 = tdskel;
 newskel1(:,1) = -tdskel(:,1);
-if strcmp(skelldef.currlayer,'all')
-    a = [0 15];
-else
-    a = 0;
+% if strcmp(skelldef.currlayer,'all')
+%     a = [0 15];
+% else
+%     a = 0;
+% end
+if ~(skelldef.length==90||skelldef.length==45)
+    error('unexpected skeleton definition. this function was just coded for 15 joint skeletons, other uses will fail. update left-right flipping, or change the skelldef to have hemibodies and change that. this is probably the simplest way to go about it, but all the conformation functions (and anything that uses skelldef) will need to be changed as well. This should probably have been defined as a class. ')
 end
+a = (0:(floor(size(tdskel,1)/15)-1))*15;
 
 for i = a
     newskel(skelldef.bodyparts.HEAD+i,:) = newskel1(skelldef.bodyparts.HEAD+i,:);
