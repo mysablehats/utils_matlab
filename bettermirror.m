7e9fb173250cea7b5943a2a6d16abf3960b0f630
mysablehats <mysablehats@gmail.com>
2018-02-27 15:18:37 +0000

    debug skeletons

--- bettermirror.m
+++ bettermirror.m
@@ -1,3 +1,7 @@
 function newskel = bettermirror(tdskel, skelldef)
 % just a wrapper for my improved mirroring function. 
 [~, newskel]  = inv_core(tdskel, skelldef, []);
+if 0
+skeldraw(tdskel(1:15,:),'f','W','m')
+skeldraw(newskel(1:15,:),'f','A','m')
+end
